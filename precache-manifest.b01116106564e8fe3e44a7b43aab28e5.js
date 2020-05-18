bdc238f27220ad21f316319c1317404a2a733136
Valentin Kazancev <qubalmail@yandex.ru>
2020-05-18 22:14:22 +0700

    Updates

---
+++ precache-manifest.b01116106564e8fe3e44a7b43aab28e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485f6a5c8b5b114a83ce44c92f38921d",
+    "url": "/framer-motion/index.html"
+  },
+  {
+    "revision": "d54312f7cdd34873b7cc",
+    "url": "/framer-motion/static/css/main.a368ccd3.chunk.css"
+  },
+  {
+    "revision": "207d616c78d46b4bcf8f",
+    "url": "/framer-motion/static/js/2.59d59301.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/framer-motion/static/js/2.59d59301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54312f7cdd34873b7cc",
+    "url": "/framer-motion/static/js/main.740e9986.chunk.js"
+  },
+  {
+    "revision": "2242e37430a3ceb8d9b5",
+    "url": "/framer-motion/static/js/runtime-main.819b303f.js"
+  }
+]);
